3e852cb5595c6807990be9ce9a80aa551b6dd3eb
Gradess <trofimovjava@gmail.com>
2023-01-03 21:28:20 +0100

    [NANI-1] Add AnkiRequestNode

---
+++ Source/AnkiIntegration/Private/AnkiRequestNode.cpp
@@ -0,0 +1,20 @@
+﻿// Copyright Gradess Games. All Rights Reserved.
+
+#include "AnkiRequestNode.h"
+#include "AnkiLibrary.h"
+#include "BlueprintJsonLibrary.h"
+#include "HttpModule.h"
+
+UAnkiRequestNode* UAnkiRequestNode::AnkiRequest(UObject* WorldContextObject, const FBlueprintJsonObject& InBody)
+{
+	auto RequestNode = NewObject<UAnkiRequestNode>();
+	RequestNode->Body = InBody;
+	return RequestNode;
+}
+
+void UAnkiRequestNode::Activate()
+{
+	Super::Activate();
+
+	UAnkiLibrary::SendRequest(Body, [&](const FBlueprintJsonObject& InResponse) { OnResponse.Broadcast(InResponse); });
+}
---
+++ Source/AnkiIntegration/Public/AnkiRequestNode.h
@@ -0,0 +1,35 @@
+﻿// Copyright Gradess Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "Kismet/BlueprintAsyncActionBase.h"
+#include "AnkiRequestNode.generated.h"
+
+class IHttpRequest;
+struct FBlueprintJsonObject;
+
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FAnkiResponse, FBlueprintJsonObject, Response);
+
+UCLASS()
+class ANKIINTEGRATION_API UAnkiRequestNode : public UBlueprintAsyncActionBase
+{
+	GENERATED_BODY()
+
+public:
+	UPROPERTY(BlueprintAssignable, Category = "AnkiRequest")
+	FAnkiResponse OnResponse;
+
+	UFUNCTION(BlueprintCallable, meta = (BlueprintInternalUseOnly = "true", WorldContext = "WorldContextObject"), Category = "AnkiRequest")
+	static UAnkiRequestNode* AnkiRequest(UObject* WorldContextObject, const FBlueprintJsonObject& InBody);
+
+	virtual void Activate() override;
+
+protected:
+	UPROPERTY(BlueprintReadOnly, Category = "AnkiRequest")
+	FBlueprintJsonObject Body;
+
+	TSharedPtr<IHttpRequest> Request;
+};
+
+
