2e24f9a27c0a66b475f87df5d1c7b94a15eeb8f9
Gradess <trofimovjava@gmail.com>
2023-01-03 20:07:04 +0100

    Init

---
+++ Source/AnkiIntegration/Private/AnkiLibrary.cpp
@@ -0,0 +1,19 @@
+﻿// Copyright Gradess Games. All Rights Reserved.
+
+
+#include "AnkiLibrary.h"
+
+#include "HttpModule.h"
+
+FBlueprintJsonObject UAnkiLibrary::SendRequest(const FString& InAction, const int32 InVersion)
+{
+	auto HttpModule = &FHttpModule::Get();
+	auto Request = HttpModule->CreateRequest();
+	Request->SetURL()
+	return UBlueprintJsonLibrary::JsonMake();
+}
+
+// void UAnkiLibrary::GetEaseFactors()
+// {
+// 	
+// }
---
+++ Source/AnkiIntegration/Public/AnkiLibrary.h
@@ -0,0 +1,37 @@
+﻿// Copyright Gradess Games. All Rights Reserved.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "BlueprintJsonLibrary.h"
+#include "UObject/Object.h"
+#include "AnkiLibrary.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class ANKIINTEGRATION_API UAnkiLibrary : public UObject
+{
+	GENERATED_BODY()
+
+public:
+	FBlueprintJsonObject SendRequest(const FString& InAction, const int32 InVersion);
+	
+	// Card Actions
+	// void GetEaseFactors();
+	// void SetEaseFactors();
+	// void SetSpecificValueOfCard();
+	// void Suspend();
+	// void Unsuspend();
+	// void Suspended();
+	// void AreSuspended();
+	// void AreDue();
+	// void GetIntervals();
+	// void FindCards();
+	// void CardsToNotes();
+	// void CardsModTime();
+	// void CardsInfo();
+	// void ForgetCards();
+	// void RelearnCards();
+};
